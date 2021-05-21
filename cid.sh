3a7d01309418e8cd8d35aec815d4c0d6fd3448f6
Benoit Lacherez <blacherez@gmail.com>
2021-05-21 21:06:44 +0200

    Ajout de la création des cgroups en début de test

--- cid.sh
+++ cid.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 . ./env.sh
 
+sudo cgcreate -g perf_event:apache
+sudo cgcreate -g perf_event:mysql
+sudo cgclassify -g perf_event:apache $(pidof apache2)
+sudo cgclassify -g perf_event:mysql $(pidof mysqld)
+
+
 . ./env_statique.sh
 sh script_test.sh
 
