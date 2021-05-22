b44647adb4293da5002b2bac7d83cbb43f146956
Benoit Lacherez <blacherez@gmail.com>
2021-05-22 22:36:28 +0200

    Correction du nombre d'exécutions

--- browser_selenium.py
+++ browser_selenium.py
@@ -9,6 +9,6 @@ url = sys.argv[2]
 with webdriver.Firefox() as driver:
     wait = WebDriverWait(driver, 10)
     driver.get(url)
-    for i in range(1, int(nb)):
+    for i in range(1, int(nb)+1):
         print("Url : %s | %s / %s" % (url, i, nb))
         driver.refresh()
